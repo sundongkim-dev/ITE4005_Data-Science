50581258e6c7aab4eb0aff7c4b056c68d33d9124
Paul_Kim <mok03127@naver.com>
2022-05-25 17:53:24 +0900

    Update clustering.py

--- DBSCAN/clustering.py
+++ DBSCAN/clustering.py
@@ -2,21 +2,27 @@ import sys
 import os
 import pandas as pd
 import numpy as np
+from collections import defaultdict
 
 def getDist(a, b):
     return np.sqrt(np.dot(a-b, a-b))
 
-def printFiles(input_file_name, clusters):
-    return 1
+def printFiles(input_file_name, n, clusters):
+    for cluster_id in range(n):
+        cluster = []
+        if cluster_id < len(clusters):
+            cluster = clusters[cluster_id]
+        output_file_name = input_file_name.split('.')[0] + '_cluster_' + str(cluster_id)+'.txt'
+        np.savetxt(output_file_name, cluster, fmt='%d\n')
 
 if __name__ == '__main__':
     if len(sys.argv) != 5:
         print("Execute the program with 4 arguments: input file name, n, Eps, MinPts")
 
     input_file_name = sys.argv[1]
-    n = sys.argv[2]         # Num of clusters
-    eps = sys.argv[3]       # Epsilon 
-    minPts = sys.argv[4]    # Minimum number of points
+    n = int(sys.argv[2])         # Num of clusters
+    eps = float(sys.argv[3])       # Epsilon 
+    minPts = int(sys.argv[4])    # Minimum number of points
 
     # Read Data 
     objects = pd.read_csv(input_file_name, sep='\t', header=None, names=['id', 'x_coordinate', 'y_coordinate'])
@@ -24,7 +30,7 @@ if __name__ == '__main__':
     
     # Sort by x coordinate
     points_list = []
-    neighbor_list = {}
+    neighbor_list = []
     for object in objects:
         object_id = int(object[0])
         object_coordinate = np.array([object[1], object[2]])
@@ -33,13 +39,25 @@ if __name__ == '__main__':
         for neighbor in reversed(points_list):
             neighbor_id = neighbor[0]
             neighbor_coordinate = neighbor[1]
-            if object_coordinate - neighbor_coordinate[0] > eps:
+            if object_coordinate[0] - neighbor_coordinate[0] > eps:
                 break
             if getDist(object_coordinate, neighbor_coordinate) <= eps:
-                neighbor_list[object_id].append(neighbor_id)
-                neighbor_list[neighbor_id].append(object_id)
+                print(object_coordinate, neighbor_coordinate)
+                print(object_id)
+                if object_id in neighbor_list:
+                    neighbor_list[object_id].append(neighbor_id)
+                else:
+                    neighbor_list[object_id] = neighbor_id
 
-        points_list.append(object_id, object_coordinate)
+                if neighbor_id in neighbor_list:
+                    neighbor_list[neighbor_id].append(neighbor_id)
+                else:
+                    neighbor_list[neighbor_id] = neighbor_id
+                
+                # neighbor_list[object_id].append(neighbor_id)
+                # neighbor_list[neighbor_id].append(object_id)
+
+        points_list.append((object_id, object_coordinate))
 
     # Generate clusters
     cluster_id = 0
@@ -69,12 +87,11 @@ if __name__ == '__main__':
                         q.append(i)
         cluster_list.append(cur_cluster)
     
-    cluster_list.sort()
-
-    cluster_id += 1
-
+    cluster_list.sort(key=lambda x : len(x), reverse=True)
+    while len(cluster_list) > n:
+        cluster_list.pop(-1)
 
     # Save file
-    result = printFiles(input_file_name, clusters)
+    printFiles(input_file_name, n, cluster_list)
 
 
