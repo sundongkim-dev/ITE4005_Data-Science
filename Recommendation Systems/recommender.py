01059976b6a3b7f9a61f4f6a553d0261523b5099
김선동 <mok03127@hanyang.ac.kr>
2022-06-02 21:14:26 +0900

    finish

--- Recommendation Systems/recommender.py
+++ Recommendation Systems/recommender.py
@@ -6,9 +6,10 @@ def get_cosine_similarity(a, i):
     same_item_idx = (a>= 0) * (i >= 0)
     if len(same_item_idx[same_item_idx]) == 0:
         return 0
+    # 양수 데이터만 남김
     vec1 = a[same_item_idx]
     vec2 = i[same_item_idx]
-    return vec1.dot(vec2) / (sum(np.sqrt(vec1 ** 2)) + sum(np.sqrt(vec2 ** 2)))
+    return vec1.dot(vec2) / (np.sqrt(sum(vec1 ** 2)) * np.sqrt(sum(vec2 ** 2)))
   
 # ./recommender.py u1.base u1.test
 if __name__ == '__main__':    
@@ -33,8 +34,7 @@ if __name__ == '__main__':
     num_of_items = max(train_set.iloc[:, 1])
 
     # rating matrix 만들기 - Df사용하면 없는 정보 누락할 수 있음
-    rating_matrix = np.full((num_of_users, num_of_items), 0)
-
+    rating_matrix = np.full((num_of_users, num_of_items), -1)
     for user_id, item_id, rating in train_set.values.tolist():
         rating_matrix[user_id-1][item_id-1] = rating
 
@@ -43,15 +43,18 @@ if __name__ == '__main__':
     for user in range(num_of_users):
         rated_item = rating_matrix[user] >= 0        
         avg_rating[user] = rating_matrix[user][rated_item].sum() / len(rated_item[rated_item])
-    print("DONE")
+    
     # 유저 간 cosine similarity로 neighbors 구하기
     user_similarity_matrix = np.ones([num_of_users, num_of_users])
     for i in range(num_of_users):
         for j in range(num_of_users):
-            user_similarity_matrix[i][j] = get_cosine_similarity(rating_matrix[i], rating_matrix[j])
-    print("DONE")
+            if i > j:
+                user_similarity_matrix[i][j] = user_similarity_matrix[j][i]
+            else:
+                user_similarity_matrix[i][j] = get_cosine_similarity(rating_matrix[i], rating_matrix[j])
+    
     # u#.base_prediction.txt에 넘겨줄 정보 만들기
-    res = list()
+    result = list()
     for user_id, item_id, rating in test_set.values.tolist():
         try:
             itemInfo = rating_matrix[:, item_id-1]
@@ -59,14 +62,14 @@ if __name__ == '__main__':
             rated_user_similarity = user_similarity_matrix[user_id-1, rated]
             # 적절한 Neighbor가 없는 경우 유저가 부여한 rating의 평균값을 취한다.
             if rated_user_similarity.sum() == 0:
-                res.append([user_id, item_id, round(avg_rating[user_id-1])])
+                result.append([user_id, item_id, avg_rating[user_id-1]])
+            # 적절한 Neighbor가 있다면, Aggregation
             else:
-                user_similarity_ratings = rated_user_similarity * (itemInfo[rated] - avg_rating[rated])
-                rat = avg_rating[user_id-1] + user_similarity_ratings.sum() / rated_user_similarity.sum()
-                res.append([user_id, item_id, round(np.clip(rat, 1, 5))])
+                prediction = avg_rating[user_id-1] + sum(rated_user_similarity * (itemInfo[rated] - avg_rating[rated])) / sum(rated_user_similarity)
+                result.append([user_id, item_id, np.clip(prediction, 1, 5)])
+        # 없던 아이템인 경우 평균값
         except IndexError as e:
-            res.append([user_id, item_id, round(avg_rating[user_id-1])])
-    print("DONE")
-    # save file
-    np.savetxt(output_file_name, res, fmt='%d\t%d\t%s')
+            result.append([user_id, item_id, avg_rating[user_id-1]])
     
+    # save file
+    np.savetxt(output_file_name, result, fmt='%d\t%d\t%s')
